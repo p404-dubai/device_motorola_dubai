b98bfba224a87c384090326bb17e54fc626f0014
Nirmal Kumar <nirmalku@codeaurora.org>
2022-12-01 18:26:37 +0530

    dubai: BoardConfig: librecovery_updater conditional compilation
    Use soong variable to differentiate between sg or bsg framework
    
    Change-Id: I99d6fd9bdd5e55547e351c7c5090ddef5b1597a3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,5 +134,12 @@ BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 
+# UFS
+#namespace definition for librecovery_updater
+#differentiate legacy 'sg' or 'bsg' framework
+SOONG_CONFIG_NAMESPACES += ufsbsg
+SOONG_CONFIG_ufsbsg += ufsframework
+SOONG_CONFIG_ufsbsg_ufsframework := bsg
+
 # inherit from the proprietary version
 include vendor/motorola/dubai/BoardConfigVendor.mk

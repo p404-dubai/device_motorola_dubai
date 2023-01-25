130843598447240698b77c5109037eac20f90214
Kunmun <kunmun@aospa.co>
2023-01-25 21:24:21 +0530

    dubai: Makefile: Drop vendor move flag
    Defined by QCOM common component
    
    Change-Id: I4e079777b459901c05ca5a491bafe3404fedfa3c

--- device.mk
+++ device.mk
@@ -560,8 +560,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
-PRODUCT_VENDOR_MOVE_ENABLED := true
-
 # WiFi Display
 PRODUCT_PACKAGES += \
     libavservices_minijail \

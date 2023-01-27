abd0a885c9a87c3fc84c9e01d6dab3ac0b6ec147
Kunmun <kunmun@aospa.co>
2023-01-27 23:02:14 +0530

    dubai: Inherit QTI USB component
    Change-Id: I785778cd8d1d59388f495170678db6437056e5ff

--- device.mk
+++ device.mk
@@ -327,7 +327,8 @@ TARGET_COMMON_QTI_COMPONENTS += \
     bt \
     display \
     gps \
-    perf
+    perf \
+    usb
 
 # RenderScript
 PRODUCT_PACKAGES += \
@@ -400,18 +401,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh
-
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
 PRODUCT_PACKAGES += \

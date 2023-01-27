ElectroPerf/android_device_motorola_dubai

dubai: Import MotoActions and it's overlays
Also merge resources from devicesettings repository
and get rid of it's dependency.

Reference:
[1] https://github.com/LineageOS/android_hardware_motorola
[2] https://github.com/LineageOS/android_packages_resources_devicesettings

Change-Id: I35429622954c57a6d06483eff95242cfb63ffeaf

MotoActions/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = "MotoActions";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.i(TAG, "Booting");
+        context.startServiceAsUser(new Intent(context, MotoActionsService.class),
+                UserHandle.CURRENT);
+    }
+}

MotoActions/src/org/lineageos/settings/device/SensorAction.java
@@ -1,0 +1,22 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device;
+
+public interface SensorAction {
+    void action();
+}

MotoActions/src/org/lineageos/settings/device/actions/ChopChopSensor.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright (c) 2015-2016 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device.actions;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.util.Log;
+
+import org.lineageos.settings.device.MotoActionsSettings;
+import org.lineageos.settings.device.SensorHelper;
+
+public class ChopChopSensor implements SensorEventListener, UpdatedStateNotifier {
+    private static final String TAG = "MotoActions-ChopChopSensor";
+
+    private final MotoActionsSettings mMotoActionsSettings;
+    private final SensorHelper mSensorHelper;
+    private final Sensor mSensor;
+    private final Sensor mProx;
+
+    private boolean mIsEnabled;
+    private boolean mProxIsCovered;
+
+    public ChopChopSensor(MotoActionsSettings motoActionsSettings, SensorHelper sensorHelper) {
+        mMotoActionsSettings = motoActionsSettings;
+        mSensorHelper = sensorHelper;
+        mSensor = sensorHelper.getChopChopSensor();
+        mProx = sensorHelper.getProximitySensor();
+    }
+
+    @Override
+    public synchronized void updateState() {
+        if (mMotoActionsSettings.isChopChopGestureEnabled() && !mIsEnabled) {
+            Log.d(TAG, "Enabling");
+            mSensorHelper.registerListener(mSensor, this);
+            mSensorHelper.registerListener(mProx, mProxListener);
+            mIsEnabled = true;
+        } else if (!mMotoActionsSettings.isChopChopGestureEnabled() && mIsEnabled) {
+            Log.d(TAG, "Disabling");
+            mSensorHelper.unregisterListener(this);
+            mSensorHelper.unregisterListener(mProxListener);
+            mIsEnabled = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "chop chop triggered");
+        if (mProxIsCovered) {
+            Log.d(TAG, "proximity sensor covered, ignoring chop-chop");
+            return;
+        }
+        mMotoActionsSettings.chopChopAction();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private final SensorEventListener mProxListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            mProxIsCovered = event.values[0] < mProx.getMaximumRange();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+}

MotoActions/src/org/lineageos/settings/device/actions/TorchAction.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device.actions;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
+import org.lineageos.settings.device.SensorAction;
+
+public class TorchAction implements SensorAction {
+    private static boolean mTorchEnabled;
+
+    private final CameraManager mCameraManager;
+    private final Vibrator mVibrator;
+
+    private String mRearCameraId;
+
+    public TorchAction(Context mContext) {
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerTorchCallback(new MyTorchCallback(), null);
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        try {
+            for (final String cameraId : mCameraManager.getCameraIdList()) {
+                CameraCharacteristics characteristics =
+                        mCameraManager.getCameraCharacteristics(cameraId);
+                int cOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (cOrientation == CameraCharacteristics.LENS_FACING_BACK) {
+                    mRearCameraId = cameraId;
+                    break;
+                }
+            }
+        } catch (CameraAccessException ignored) {
+        }
+    }
+
+    @Override
+    public void action() {
+        mVibrator.vibrate(VibrationEffect.createOneShot(250, VibrationEffect.DEFAULT_AMPLITUDE));
+        if (mRearCameraId != null) {
+            try {
+                mCameraManager.setTorchMode(mRearCameraId, !mTorchEnabled);
+                mTorchEnabled = !mTorchEnabled;
+            } catch (CameraAccessException ignored) {
+            }
+        }
+    }
+
+    private class MyTorchCallback extends CameraManager.TorchCallback {
+
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            if (!cameraId.equals(mRearCameraId)) {
+                return;
+            }
+            mTorchEnabled = enabled;
+        }
+
+        @Override
+        public void onTorchModeUnavailable(String cameraId) {
+            if (!cameraId.equals(mRearCameraId)) {
+                return;
+            }
+            mTorchEnabled = false;
+        }
+    }
+}

MotoActions/src/org/lineageos/settings/device/doze/FlatUpSensor.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (C) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device.doze;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.util.Log;
+
+import org.lineageos.settings.device.MotoActionsSettings;
+import org.lineageos.settings.device.SensorAction;
+import org.lineageos.settings.device.SensorHelper;
+
+public class FlatUpSensor implements ScreenStateNotifier {
+    private static final String TAG = "MotoActions-FlatUpSensor";
+
+    private final MotoActionsSettings mMotoActionsSettings;
+    private final SensorHelper mSensorHelper;
+    private final SensorAction mSensorAction;
+    private final Sensor mFlatUpSensor;
+    private final Sensor mStowSensor;
+
+    private boolean mEnabled;
+    private boolean mIsStowed;
+    private boolean mLastFlatUp;
+
+    public FlatUpSensor(MotoActionsSettings MotoActionsSettings, SensorHelper sensorHelper,
+                        SensorAction action) {
+        mMotoActionsSettings = MotoActionsSettings;
+        mSensorHelper = sensorHelper;
+        mSensorAction = action;
+
+        mFlatUpSensor = sensorHelper.getFlatUpSensor();
+        mStowSensor = sensorHelper.getStowSensor();
+    }
+
+    @Override
+    public void screenTurnedOn() {
+        if (mEnabled) {
+            Log.d(TAG, "Disabling");
+            mSensorHelper.unregisterListener(mFlatUpListener);
+            mSensorHelper.unregisterListener(mStowListener);
+            mEnabled = false;
+        }
+    }
+
+    @Override
+    public void screenTurnedOff() {
+        if (mMotoActionsSettings.isPickUpEnabled() && !mEnabled) {
+            Log.d(TAG, "Enabling");
+            mSensorHelper.registerListener(mFlatUpSensor, mFlatUpListener);
+            mSensorHelper.registerListener(mStowSensor, mStowListener);
+            mEnabled = true;
+        }
+    }
+
+    private final SensorEventListener mFlatUpListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            boolean thisFlatUp = (event.values[0] != 0);
+
+            Log.d(TAG, "event: " + thisFlatUp + " mLastFlatUp=" + mLastFlatUp + " mIsStowed=" +
+                    mIsStowed);
+
+            if (mLastFlatUp && !thisFlatUp && !mIsStowed) {
+                mSensorAction.action();
+            }
+            mLastFlatUp = thisFlatUp;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+
+    private final SensorEventListener mStowListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            mIsStowed = (event.values[0] != 0);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+}

MotoActions/src/org/lineageos/settings/device/doze/ScreenStateNotifier.java
@@ -1,0 +1,24 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device.doze;
+
+public interface ScreenStateNotifier {
+    void screenTurnedOn();
+
+    void screenTurnedOff();
+}

p404-dubai/device_motorola_dubai

MotoActions/src/co/aospa/settings/device/BootCompletedReceiver.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.settings.device;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = "MotoActions";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.i(TAG, "Booting");
+        context.startServiceAsUser(new Intent(context, MotoActionsService.class),
+                UserHandle.CURRENT);
+    }
+}

MotoActions/src/co/aospa/settings/device/SensorAction.java
@@ -1,0 +1,22 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.settings.device;
+
+public interface SensorAction {
+    void action();
+}

MotoActions/src/co/aospa/settings/device/actions/ChopChopSensor.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright (c) 2015-2016 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.settings.device.actions;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.util.Log;
+
+import co.aospa.settings.device.MotoActionsSettings;
+import co.aospa.settings.device.SensorHelper;
+
+public class ChopChopSensor implements SensorEventListener, UpdatedStateNotifier {
+    private static final String TAG = "MotoActions-ChopChopSensor";
+
+    private final MotoActionsSettings mMotoActionsSettings;
+    private final SensorHelper mSensorHelper;
+    private final Sensor mSensor;
+    private final Sensor mProx;
+
+    private boolean mIsEnabled;
+    private boolean mProxIsCovered;
+
+    public ChopChopSensor(MotoActionsSettings motoActionsSettings, SensorHelper sensorHelper) {
+        mMotoActionsSettings = motoActionsSettings;
+        mSensorHelper = sensorHelper;
+        mSensor = sensorHelper.getChopChopSensor();
+        mProx = sensorHelper.getProximitySensor();
+    }
+
+    @Override
+    public synchronized void updateState() {
+        if (mMotoActionsSettings.isChopChopGestureEnabled() && !mIsEnabled) {
+            Log.d(TAG, "Enabling");
+            mSensorHelper.registerListener(mSensor, this);
+            mSensorHelper.registerListener(mProx, mProxListener);
+            mIsEnabled = true;
+        } else if (!mMotoActionsSettings.isChopChopGestureEnabled() && mIsEnabled) {
+            Log.d(TAG, "Disabling");
+            mSensorHelper.unregisterListener(this);
+            mSensorHelper.unregisterListener(mProxListener);
+            mIsEnabled = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "chop chop triggered");
+        if (mProxIsCovered) {
+            Log.d(TAG, "proximity sensor covered, ignoring chop-chop");
+            return;
+        }
+        mMotoActionsSettings.chopChopAction();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private final SensorEventListener mProxListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            mProxIsCovered = event.values[0] < mProx.getMaximumRange();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+}

MotoActions/src/co/aospa/settings/device/actions/TorchAction.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.settings.device.actions;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
+import co.aospa.settings.device.SensorAction;
+
+public class TorchAction implements SensorAction {
+    private static boolean mTorchEnabled;
+
+    private final CameraManager mCameraManager;
+    private final Vibrator mVibrator;
+
+    private String mRearCameraId;
+
+    public TorchAction(Context mContext) {
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerTorchCallback(new MyTorchCallback(), null);
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        try {
+            for (final String cameraId : mCameraManager.getCameraIdList()) {
+                CameraCharacteristics characteristics =
+                        mCameraManager.getCameraCharacteristics(cameraId);
+                int cOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (cOrientation == CameraCharacteristics.LENS_FACING_BACK) {
+                    mRearCameraId = cameraId;
+                    break;
+                }
+            }
+        } catch (CameraAccessException ignored) {
+        }
+    }
+
+    @Override
+    public void action() {
+        mVibrator.vibrate(VibrationEffect.createOneShot(250, VibrationEffect.DEFAULT_AMPLITUDE));
+        if (mRearCameraId != null) {
+            try {
+                mCameraManager.setTorchMode(mRearCameraId, !mTorchEnabled);
+                mTorchEnabled = !mTorchEnabled;
+            } catch (CameraAccessException ignored) {
+            }
+        }
+    }
+
+    private class MyTorchCallback extends CameraManager.TorchCallback {
+
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            if (!cameraId.equals(mRearCameraId)) {
+                return;
+            }
+            mTorchEnabled = enabled;
+        }
+
+        @Override
+        public void onTorchModeUnavailable(String cameraId) {
+            if (!cameraId.equals(mRearCameraId)) {
+                return;
+            }
+            mTorchEnabled = false;
+        }
+    }
+}

MotoActions/src/co/aospa/settings/device/doze/FlatUpSensor.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (C) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.settings.device.doze;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.util.Log;
+
+import co.aospa.settings.device.MotoActionsSettings;
+import co.aospa.settings.device.SensorAction;
+import co.aospa.settings.device.SensorHelper;
+
+public class FlatUpSensor implements ScreenStateNotifier {
+    private static final String TAG = "MotoActions-FlatUpSensor";
+
+    private final MotoActionsSettings mMotoActionsSettings;
+    private final SensorHelper mSensorHelper;
+    private final SensorAction mSensorAction;
+    private final Sensor mFlatUpSensor;
+    private final Sensor mStowSensor;
+
+    private boolean mEnabled;
+    private boolean mIsStowed;
+    private boolean mLastFlatUp;
+
+    public FlatUpSensor(MotoActionsSettings MotoActionsSettings, SensorHelper sensorHelper,
+                        SensorAction action) {
+        mMotoActionsSettings = MotoActionsSettings;
+        mSensorHelper = sensorHelper;
+        mSensorAction = action;
+
+        mFlatUpSensor = sensorHelper.getFlatUpSensor();
+        mStowSensor = sensorHelper.getStowSensor();
+    }
+
+    @Override
+    public void screenTurnedOn() {
+        if (mEnabled) {
+            Log.d(TAG, "Disabling");
+            mSensorHelper.unregisterListener(mFlatUpListener);
+            mSensorHelper.unregisterListener(mStowListener);
+            mEnabled = false;
+        }
+    }
+
+    @Override
+    public void screenTurnedOff() {
+        if (mMotoActionsSettings.isPickUpEnabled() && !mEnabled) {
+            Log.d(TAG, "Enabling");
+            mSensorHelper.registerListener(mFlatUpSensor, mFlatUpListener);
+            mSensorHelper.registerListener(mStowSensor, mStowListener);
+            mEnabled = true;
+        }
+    }
+
+    private final SensorEventListener mFlatUpListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            boolean thisFlatUp = (event.values[0] != 0);
+
+            Log.d(TAG, "event: " + thisFlatUp + " mLastFlatUp=" + mLastFlatUp + " mIsStowed=" +
+                    mIsStowed);
+
+            if (mLastFlatUp && !thisFlatUp && !mIsStowed) {
+                mSensorAction.action();
+            }
+            mLastFlatUp = thisFlatUp;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+
+    private final SensorEventListener mStowListener = new SensorEventListener() {
+        @Override
+        public synchronized void onSensorChanged(SensorEvent event) {
+            mIsStowed = (event.values[0] != 0);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor mSensor, int accuracy) {
+        }
+    };
+}

MotoActions/src/co/aospa/settings/device/doze/ScreenStateNotifier.java
@@ -1,0 +1,24 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (c) 2017-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.settings.device.doze;
+
+public interface ScreenStateNotifier {
+    void screenTurnedOn();
+
+    void screenTurnedOff();
+}
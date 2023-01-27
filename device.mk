a0dd946292266062c1d59042cf37703e7ddc76b5
Kunmun <kunmun@aospa.co>
2023-01-27 19:41:06 +0530

    dubai: Inherit QTI Bluetooth component
    • Drop duplicate product packages, properties and flags.
    • Revert back to Bluetooth Audio HIDL implementation.
    
    Change-Id: I19c51bc30edb2d132a56596f586edd9d445b3024

--- device.mk
+++ device.mk
@@ -51,8 +51,7 @@ PRODUCT_COPY_FILES += \
 
 # Ant
 PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant@1.0.vendor
+    AntHalService-Soong
 
 # Atrace
 PRODUCT_PACKAGES += \
@@ -61,8 +60,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl \
-    audio.bluetooth.default \
     libaudiopreprocessing \
     libbundlewrapper \
     libdownmix \
@@ -92,17 +89,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 TARGET_USES_FOD_ZPOS := true
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Boot control
 PRODUCT_PACKAGES += \
@@ -386,7 +372,8 @@ PRODUCT_PACKAGES += \
 # QTI Components
 TARGET_COMMON_QTI_COMPONENTS += \
     audio \
-    av
+    av \
+    bt
 
 # QTI service tracker
 PRODUCT_PACKAGES += \
